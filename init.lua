079ab31ded5b8c6791d6ab3c94eb22493d5027fb
Rindel Subaan <valsubaan@yahoo.com>
2024-08-03 22:20:18 +0800

    Added Treesitter and its configs

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 require('settings')
 require('color-schemes')
+require('plugins')
+
+require('plugin-configs.treesitter-config')
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,8 @@
-return require'packer'.startup(function()
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim' -- Neovim plugin manager
     use 'ellisonleao/gruvbox.nvim' -- sweet gruvbox colorscheme
+
+    --> TREESITTER PLUGINS
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use 'nvim-treesitter/nvim-treesitter-refactor' -- refactor modules for nvim treesitter
 end)
