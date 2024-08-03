cf422245ac33154a59c1e6f4cd495ed6f84e7c90
Rindel Subaan <valsubaan@yahoo.com>
2024-08-03 19:03:48 +0800

    Modify mappings for nvim cmp

--- lua/lsp-config/nvim_cmp.lua
+++ lua/lsp-config/nvim_cmp.lua
@@ -6,10 +6,10 @@ cmp.setup({
     {name = 'nvim_lsp'},
   },
   mapping = {
-    ['<C-y>'] = cmp.mapping.confirm({select = false}),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    ['<C-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<Tab>'] = cmp.mapping.select_next_item({behavior = 'select'}),
     ['<C-p>'] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item({behavior = 'insert'})
