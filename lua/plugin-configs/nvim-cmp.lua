6714cb2133008bfcf550fd14d6e845c85e12f16b
Rindel Subaan <valsubaan@yahoo.com>
2024-08-05 16:27:31 +0800

    Add mappings in nvim-cmp config file.

--- lua/plugin-configs/nvim-cmp.lua
+++ lua/plugin-configs/nvim-cmp.lua
@@ -10,6 +10,21 @@ cmp.setup({
       require('luasnip').lsp_expand(args.body)
     end,
   },
+  -- mappings for autocompletion
+  mapping = {
+      ['<C-n>'] = cmp.mapping.select_next_item(),
+      ['<C-p>'] = cmp.mapping.select_prev_item(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<CR>'] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+      }),
+      ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+  },
   -- lspkind config
   formatting = {
     format = lspkind.cmp_format({
